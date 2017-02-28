f3e6d84427ab98881eae11471a76d88be3153b30
Alexander <redandspring@gmail.com>
2017-02-28 18:09:10 +0300

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `User`;
 
 CREATE TABLE IF NOT EXISTS `User` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
@@ -14,22 +14,22 @@ CREATE TABLE IF NOT EXISTS `User` (
 --
 
 INSERT INTO `User` (`id`, `name`, `age`, `isAdmin`, `createdDate`) VALUES
-(1, 'Andrey Mironov', 29, 1, '2017-02-27 11:01:42'),
-(2, 'Aleksey Andreev', 31, 1, '2017-02-27 11:01:42'),
-(3, 'Alexander Ivanov', 38, 1, '2017-02-27 11:01:42'),
-(4, 'Semen Sergeev', 26, 1, '2017-02-27 11:01:42'),
-(5, 'Tyrion Lannister', 40, 0, '2017-02-27 11:01:42'),
-(7, 'Kit Harington', 42, 0, '2017-02-27 11:01:42'),
-(8, 'Charles Dance', 40, 0, '2017-02-27 11:01:42'),
-(9, 'Theon Greyjoy', 42, 0, '2017-02-27 11:01:42'),
-(10, 'Iain Glen', 43, 0, '2017-02-27 11:01:42'),
-(11, 'Robb Stark', 24, 0, '2017-02-27 11:01:42'),
-(12, 'Mark Addy', 24, 0, '2017-02-27 11:01:42'),
-(13, 'Samwell Tarly', 35, 0, '2017-02-27 11:01:42'),
-(14, 'Conleth Hill', 36, 0, '2017-02-27 11:01:42'),
-(15, 'Jerome Flynn', 32, 0, '2017-02-27 11:01:42'),
-(16, 'Liam Cunningham', 37, 0, '2017-02-27 11:01:42'),
-(17, 'Davos Seaworth', 38, 0, '2017-02-27 11:01:42'),
-(18, 'Daniel Portman', 39, 0, '2017-02-27 11:01:42'),
-(19, 'Podrick Payne', 38, 0, '2017-02-27 11:01:42'),
-(20, 'Barristan Selmy', 50, 0, '2017-02-27 11:01:42');
+(1, 'Andrey Mironov', 29, 1, '2001-03-14 11:01:40'),
+(2, 'Aleksey Andreev', 31, 1, '2007-05-11 12:11:41'),
+(3, 'Alexander Ivanov', 38, 1, '2007-07-22 09:21:25'),
+(4, 'Semen Sergeev', 26, 1, '2009-02-26 10:01:32'),
+(5, 'Tyrion Lannister', 40, 0, '2010-03-17 12:22:40'),
+(7, 'Kit Harington', 42, 0, '2011-04-21 11:32:45'),
+(8, 'Charles Dance', 40, 0, '2014-02-27 11:42:44'),
+(9, 'Theon Greyjoy', 42, 0, '2015-12-17 03:15:41'),
+(10, 'Iain Glen', 43, 0, '2016-05-20 02:11:52'),
+(11, 'Robb Stark', 24, 0, '2016-11-17 01:01:22'),
+(12, 'Mark Addy', 24, 0, '2016-12-01 10:43:02'),
+(13, 'Samwell Tarly', 35, 0, '2016-12-27 13:01:12'),
+(14, 'Conleth Hill', 36, 0, '2017-01-30 11:01:44'),
+(15, 'Jerome Flynn', 32, 0, '2017-02-20 14:03:12'),
+(16, 'Liam Cunningham', 37, 0, '2017-02-21 15:05:22'),
+(17, 'Davos Seaworth', 38, 0, '2017-02-22 21:07:41'),
+(18, 'Daniel Portman', 39, 0, '2017-02-23 21:01:32'),
+(19, 'Podrick Payne', 38, 0, '2017-02-24 10:31:40'),
+(20, 'Barristan Selmy', 50, 0, '2017-02-25 11:12:42');
